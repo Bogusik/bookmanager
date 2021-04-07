527acf90a91735fba770bb52f1f69f44a88b7329
Bohuslav Semenov <semenov0310@gmail.com>
2021-04-07 23:24:51 +0300

    Added all urls to django

--- bookmanager/urls.py
+++ bookmanager/urls.py
@@ -2,18 +2,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls.static import static
+from django.conf import settings
 
 from books.views import BooksViewSet
 from users.views import UsersViewSet
 
+
 router = routers.DefaultRouter()
+
 
 router.register(r'books', BooksViewSet, basename='books')
 router.register(r'users', UsersViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/auth/', include('rest_framework.urls')),
+    path('api/users/auth/', obtain_jwt_token),
     path('api/', include(router.urls)),
-    path('', TemplateView.as_view(template_name='index.html'), name='index')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+paths = ['', 'login/', 'add/', 'edit/<int:pk>']
+urlpatterns.extend(
+    [path(x, TemplateView.as_view(template_name='index.html')) for x in paths]
+)
