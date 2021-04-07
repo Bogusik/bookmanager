ec39762457f28ae0bf058481282de1868bbd5c18
Bohuslav Semenov <semenov0310@gmail.com>
2021-04-07 23:27:33 +0300

    Configured router, added middleware

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import store from '../store'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -11,12 +15,50 @@ const routes = [
     path: '/',
     name: 'Books',
     component: () => import('../views/Books.vue')
+  },
+  {
+    path: '/edit/:id',
+    name: 'EditBook',
+    component: () => import('../views/EditBook.vue')
+  },
+  {
+    path: '/add',
+    name: 'AddBook',
+    component: () => import('../views/AddBook.vue')
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory('/'),
+const router = new VueRouter({
+  mode: 'history',
+  base: '/',
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  if (to.name !== 'Login' && localStorage.getItem('token')) {
+    try {
+      const resp = await Vue.axios.get('/users/me/', { headers: { Authorization: "JWT " + localStorage.getItem('token') }})
+      if (resp.status == 200) {
+        store.commit('setUser', resp.data)
+        next()
+      } else {
+        localStorage.removeItem('token')
+        next({ name: 'Login' })
+      }
+    } catch {
+      localStorage.removeItem('token')
+      next({ name: 'Login' })
+    }
+  } else {
+    if (localStorage.getItem('token')) {
+      localStorage.removeItem('token')
+    }
+    if (to.name === 'Login') {
+      next()
+    }
+    next({ name: 'Login' })
+  }
+})
+
 
 export default router
